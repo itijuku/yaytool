743fd91f3dc080f03af97b4f0a315fee41674d27
itijuku <kanjukuitijuku@gmail.com>
2025-10-20 11:21:06 +0900

    構造絶対おかしい...

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import yaytool as yt
+
+bot = yt.Login("mail","pass")
+
+bot.post()
---
+++ src/user.py
@@ -0,0 +1,6 @@
+class _post:
+    def __init__(self):
+        pass
+    
+    def post(self,text:str):
+        pass
